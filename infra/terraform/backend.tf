0449d6c375c2121aa126792f46e8adf107461958
Adepitan <akoredeadetunji93@gmail.com>
2025-12-08 19:40:39 +0100

    Chore: Setting up workflows

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "hng-state-bucket-${random_string.bucket_suffix.result}"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
